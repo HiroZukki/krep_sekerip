b5003d0bc9a17942d3858df18a13c7dede6977e0
Grayfia <dinarysf276@gmail.com>
2026-07-03 23:31:36 +0700

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b test --git-lfs
+repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 /opt/crave/resync.sh # sync source
 
+rm -rf device/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
@@ -10,12 +11,5 @@ export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
 lunch lineage_earth-bp4a-userdebug
+make installclean
 mka bacon
-
-# Upload to gofile
-echo "Upload to gofile will be started..."
-if [ -f out/target/product/earth/*.zip ]; then
-  wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-  chmod +x upload.sh ; ./upload.sh out/target/product/earth/*202607*.zip
-fi
-echo "hame"
