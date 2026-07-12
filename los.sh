9cc9fdbcb91cb7da3f89e53aeb4ce287e68ae471
Grayfia <dinarysf276@gmail.com>
2026-07-12 21:10:59 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -21,7 +21,7 @@ mka bacon
 # Upload to gofile
 echo "Upload to gofile will be started..."
 if [ -f out/target/product/earth/*202607*.zip ]; then
-    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
-    chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img ; ./upload.sh out/target/product/earth/*202607*.zip 
+  wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+  chmod +x upload.sh ; ./upload.sh out/target/product/earth/boot.img ; ./upload.sh out/target/product/earth/*202607*.zip 
 fi
 echo "hame"
