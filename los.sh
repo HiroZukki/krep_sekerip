3d2011ba00739d89c96b63ed96b70ff07c8cde3a
Grayfia <dinarysf276@gmail.com>
2026-07-27 11:55:17 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b test --git-lfs
-# /opt/crave/resync.sh # sync source
+repo init --depth=1 -u https://github.com/SilverEuphonium/android_manifest.git -b 16.0 --git-lfs --no-clone-bundle
+/opt/crave/resync.sh # sync source
 
-rm -rf device/xiaomi/earth vendor/lineage-priv/keys
-git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
-
-# Edit Source 
-rm -rf vendor/prebuilts vendor/lineage
-git clone --depth=1 https://github.com/SilverEuphonium/vendor_lineage.git -b 16.2 vendor/lineage
+git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b crDroid-16.2 device/xiaomi/earth
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
 
 . build/envsetup.sh
-lunch lineage_earth-bp4a-userdebug
-make installclean
-mka bacon
+brunch earth userdebug
 
 # Upload to gofile
 echo "Upload to gofile will be started..."
