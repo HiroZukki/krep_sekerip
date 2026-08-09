2c47e87279d1dd803ab6e477f1f96879e2695e42
Suzumiya <dinarysf276@gmail.com>
2026-08-09 23:21:58 +0700

    Update los.sh

--- los.sh
+++ los.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 # init rom source 
-repo init -u https://github.com/Kitauji-High-School/lunaris_manifests.git -b 16.2 --git-lfs --depth=1
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs --depth=1
 /opt/crave/resync.sh # sync source
 
 # Device sources
 rm -rf device/xiaomi/earth vendor/xiaomi/earth kernel/xiaomi/earth
 git clone https://github.com/Kitauji-High-School/android_device_xiaomi_earth.git -b Lunaris-16.2 device/xiaomi/earth
+
+# Patching FWB
+cd frameworks/base
+wget https://raw.githubusercontent.com/AbuRider/scripts/refs/heads/main/fwb.patch ; git am fwb.patch
+cd ../..
+
+# Custom sources
+rm -rf vendor/lineage
+git clone https://github.com/Kitauji-High-School/vendor_lineage.git -b 16.2 vendor/lineage --depth=1
 
 export BUILD_USERNAME=kumiko
 export BUILD_HOSTNAME=kitauji_quartet
