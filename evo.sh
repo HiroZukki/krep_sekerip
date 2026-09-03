35d6df549a171b39dbd79813e229d84ec6d89d86
Hirokawa Uzuki <246077556+HiroZukki@users.noreply.github.com>
2026-09-03 20:05:57 +0700

    Update evo.sh

--- evo.sh
+++ evo.sh
@@ -6,7 +6,7 @@
 
 # Clean up device source
 rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth
-rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr vendor/mediatek/ims
+rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 # device source
 git clone https://github.com/HiroZukki/device_xiaomi_earth.git -b EvolutionX-17 device/xiaomi/earth --depth=1
