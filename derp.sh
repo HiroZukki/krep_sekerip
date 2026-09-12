85e302ca4acd19d67c407397971c606e17568388
Hirokawa Uzuki <dreamsolister26@proton.me>
2026-09-13 03:14:03 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # repo init
-repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 17 --git-lfs --depth=1
+# repo init -u https://github.com/DerpFest-AOSP/android_manifest.git -b 17 --git-lfs --depth=1
 
 # Sync
-/opt/crave/resync.sh # sync source
-repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
+# /opt/crave/resync.sh # sync source
+# repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For fixing sync error
 
 # device source
+rm -rf device/xiaomi/earth
 git clone https://github.com/HiroZukki/device_xiaomi_earth.git -b DerpFest-17 device/xiaomi/earth
 
 # Patch build soong for finishing soong ninja
-cd build/soong
-wget https://raw.githubusercontent.com/HiroZukki/krep_sekerip/refs/heads/main/soong.patch
-patch -p1 < soong.patch ; rm -rf soong.patch
-cd ../..
+# cd build/soong
+# wget https://raw.githubusercontent.com/HiroZukki/krep_sekerip/refs/heads/main/soong.patch
+# patch -p1 < soong.patch ; rm -rf soong.patch
+# cd ../..
 
 # build start
 . build/envsetup.sh
