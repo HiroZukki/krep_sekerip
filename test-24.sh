033c3f2751df24bb2efa3b2afcbd24eb5dcaace0
Hirokawa Uzuki <dreamsolister26@proton.me>
2026-09-16 08:40:09 +0700

    Update test-24.sh

--- test-24.sh
+++ test-24.sh
@@ -9,6 +9,14 @@ repo sync -c --force-sync --force-remove-dirty --no-tags --no-clone-bundle # For
 
 # device source
 git clone https://github.com/MinamiQuartet/android_device_xiaomi_earth.git -b lineage-24.0 device/xiaomi/earth
+git clone https://github.com/MinamiQuartet/vendor_xiaomi_earth.git -b lineage-24.0-ims vendor/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/android_kernel_xiaomi_earth.git -b lineage-24.0 kernel/xiaomi/earth --depth=1
+git clone https://github.com/MinamiQuartet/minami-sign.git -b keys vendor/lineage-priv/keys
+
+# Hardware Repos
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-24.0 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-24.0 device/mediatek/sepolicy_vndr
 
 # build start
 . build/envsetup.sh
@@ -18,8 +26,8 @@ export BUILD_HOSTNAME=minami_quartet
 export SOONG_NINJA=ninja
 
 # start build
-breakfast earth userdebug
-m pixelos
+lunch lineage_earth-cp2a-userdebug
+mka bacon
 
 # Upload files to gofile
 echo "Upload to gofile will be started..."
